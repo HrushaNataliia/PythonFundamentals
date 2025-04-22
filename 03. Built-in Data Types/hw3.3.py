9bc2bc5972ddeadaab8c215a73c907a46119680e
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-22 19:29:43 +0300

    hw3.3

---
+++ 03. Built-in Data Types/hw3.3.py
@@ -0,0 +1,12 @@
+"""Interchange the values of two variables without using the third variable."""
+
+
+a = input("Enter the first variable (a): ")     
+b = input("Enter the second variable (b): ")
+
+print(f"Before swapping: a = {a}, b = {b}")
+
+a, b = b, a 
+
+print(f"After swapping: a = {a}, b = {b}")
+
