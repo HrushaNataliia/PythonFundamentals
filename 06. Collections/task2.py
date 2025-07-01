dfe8fa04e75067d9fbc2de0aa9eead226a750b01
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-07-01 18:26:41 +0300

    hw 6 task 2

---
+++ 06. Collections/task2.py
@@ -0,0 +1,13 @@
+"""Task2. Write a script that checks the login that the user enters.
+If the login is "First", then greet the users. If the login is
+different, send an error message.
+(need to use loop while)"""
+
+while True:
+    login = input("Enter your login: ")
+
+    if login == "First":
+        print("Welcome back, First!")
+        break  # Exit the loop if correct login
+    else:
+        print("Error: Invalid login. Please try again.")
